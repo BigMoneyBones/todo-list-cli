0f4324646fc227c7b094584c37dfbb6de0e53f12
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-02-22 22:38:41 -0600

    updated

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const prompt = require('prompt-sync')({sigint: true});
 
 let toDoList = [];
-let choice = 0;
 let newItem = [];
+
+let choice = 0;
 let count = 0;
-let status = [false, true];
+
+let status = [];
+let newStatus = [];
+
+let trueFalse = [];
 
 //1=add 2=complete 3=exit 
 
@@ -18,6 +23,12 @@ console.log("Your to-do list is empty.");
 
 while(choice !== 3) {
 
+    if (toDoList.length <= 0) {
+
+        console.log("There are no items in your to-do list.");
+
+    }
+
     console.log("\n");
     
     console.log("Select an action: ");
@@ -26,9 +37,13 @@ while(choice !== 3) {
 
     console.log("[1] Add a to-do item to the list. ");
     console.log("[2] Mark a to-do item as complete. ");
-    console.log("[3] Exit the program.");
+    console.log("[3] Exit the program. ");
+    console.log("[4] View to-do list. ")
+    
+    console.log("\n");
 
     let choice = (Number(prompt("> ")));
+
     console.log("\n");
 
     if (choice === 1) {
@@ -46,8 +61,20 @@ while(choice !== 3) {
         console.log("You have " + Number(toDoList.length) + " item(s) left to do.")
 
         for (let count = 0; count < toDoList.length; count++) {
+            
+            trueFalse[count] = false;
 
-            console.log(Number(count+1) + "." + "[" + status + "] " + toDoList[count]);
+            if (trueFalse[count] === false) {
+
+                status[count] = "incomplete";
+
+            } else if (trueFalse[count] === true) {
+
+                status[count] = "complete";
+
+            }
+
+            console.log(Number(count + 1) + "." + "[" + status[count] + "] " + toDoList[count]);
         
         }
 
@@ -56,13 +83,34 @@ while(choice !== 3) {
     if (choice === 2) {
 
         console.log("Select an item to be marked complete.");
-               
 
+        let newStatus = Number(prompt("> "));
+
+        console.log("You have " + Number(toDoList.length) + " item(s) to do." )
+
+        for (let count = 0; count < toDoList.length; count++) {
+
+            trueFalse[newStatus-1] = true;
+
+            if (trueFalse[count] === false) {
+
+                status[count] = "[incomplete]";
+
+            } else if (trueFalse[count] === true) {
+
+                status[count] = "[complete]";
+
+            }
+
+            console.log(Number(count + 1) + "." + status[count] + toDoList[count]);
+
+        }
+    
     }
     
     if (choice === 3) {
 
-        return console.log("Goodbye!");
+        return console.log("Program terminated. Goodbye!");
 
     }
 
@@ -72,7 +120,7 @@ while(choice !== 3) {
 
         for (let count = 0; count < toDoList.length; count++){
         
-        console.log(Number(count+1) + "." + "[" + status + "]" + toDoList[count]);
+        console.log(Number(count + 1) + "." + "[" + status[count] + "]" + toDoList[count]);
 
         }
 
